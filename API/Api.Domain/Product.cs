203be91c8050bfc7962e39168554c9296bbce4ed
GhazeefaFatima <me.ghazeefa.fatima@gmail.com>
2022-04-23 21:23:44 +0500

    first commit

---
+++ API/Api.Domain/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.Domain
+{
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public bool IsMainPageItem { get; set; }
+        public bool IsNewProduct { get; set; }
+        public bool IsBestSelling  { get; set; }
+        public bool IsFeaturedProduct { get; set; }
+        public bool IsOnSale { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
